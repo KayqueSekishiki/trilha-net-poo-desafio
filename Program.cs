feb85593bcf8d37054ab8302028ea44038c416fb
Kayque Lopes <104032451+KayqueSekishiki@users.noreply.github.com>
2023-12-18 17:47:42 -0300

    Finish App!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "01020", modelo: "G60 5G", imei: "111222333", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Sketchbook");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "54321", modelo: "XV Plus", imei: "333111222", memoria: 256);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Procreate Pocket");
